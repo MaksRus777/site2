86a168ecce114086a397ff67da081f8b115528c1
maksimmizyuk <mizyuk.maks@mail.ru>
2024-05-26 15:48:26 -0400

    сайт бетта, создана модель бд sclite

---
+++ myapp1/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Worker(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    sacond_name=models.CharField(max_length=35,blank=False)
+    salary=models.IntegerField(default=0)
